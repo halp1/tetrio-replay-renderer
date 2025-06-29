74f901a339fb6cb831a43f197197c9e83331323c
halp <haelpf@gmail.com>
2025-06-29 15:39:56 -0400

    fix the test

--- test/main.test.ts
+++ test/main.test.ts
@@ -2,31 +2,33 @@ import { renderer } from "../src/renderer";
 import path from "node:path";
 import fs from "node:fs/promises";
 
-const config = JSON.parse(
-  await fs.readFile(path.join(__dirname, "./config.ttc"), "utf-8")
-);
+import { test, expect } from "bun:test";
 
-await renderer.ready;
+test("renderer", async () => {
+  const config = JSON.parse(
+    await fs.readFile(path.join(__dirname, "./config.ttc"), "utf-8")
+  );
 
-renderer.configure({
-  token: process.env.TOKEN,
-  config,
+  await renderer.ready;
+
+  renderer.configure({
+    token: process.env.TOKEN,
+    config,
+  });
+
+  const outputPaths = await renderer.render(
+    JSON.parse(await fs.readFile(path.join(__dirname, "./replay.ttrm"), "utf-8")),
+    [
+      {
+        round: 0,
+        start: 100,
+        end: 1000,
+      },
+    ]
+  );
+
+  await renderer.terminate();
+	expect(outputPaths).toHaveLength(1);
+}, {
+	timeout: 120_000,
 });
-
-const outputPaths = await renderer.render(
-  JSON.parse(await fs.readFile(path.join(__dirname, "./replay.ttrm"), "utf-8")),
-  [
-    {
-      round: 0,
-      start: 100,
-      end: 1000,
-    },
-  ]
-);
-
-console.log(
-  "Render complete. Output files:",
-  outputPaths.map((p) => renderer.STORAGE_FOLDER + "/" + p).join("\n")
-);
-
-renderer.terminate();
